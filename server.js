49a4b0f309a9e0e5824b62915e63f52fb2b3ba3d
Eadroma <boukottaya.jr@gmail.com>
2022-05-09 11:34:12 +0200

    [ADD] procfile

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 // import dotenv then use it to get the port from the .env file
-require('dotenv').config();
+require("dotenv").config();
 
-app.set('port', process.env.BACK_PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 const server = http.createServer(app);
-server.listen(app.get('port'), () => {
-    console.log(`Express server listening on port ${app.get('port')}`);
+server.listen(app.get("port"), () => {
+  console.log(`Express server listening on port ${app.get("port")}`);
 });
