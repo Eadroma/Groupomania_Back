637c8eac9a49cd3c02d2761b8b2acbcc0008df46
Eadroma <boukottaya.jr@gmail.com>
2022-05-16 12:09:06 +0200

    make it better

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = require("./app");
 // import dotenv then use it to get the port from the .env file
 require("dotenv").config();
 
-app.set("port", process.env.BACK_PORT || 3000);
+app.set("port", 3000 || process.env.BACK_PORT);
 const server = http.createServer(app);
+
 server.listen(app.get("port"), () => {
   console.log(`Express server listening on port ${app.get("port")}`);
 });
